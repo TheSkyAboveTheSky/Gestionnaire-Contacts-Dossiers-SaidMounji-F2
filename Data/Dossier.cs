efb5d873bfdbb4bac98e7c6a4202a7e23dacdbde
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-02-04 21:51:56 +0100

    add Dossier Fichier Classes

---
+++ Data/Dossier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Dossier
+    {
+        private readonly List<Fichier> fichiers = new List<Fichier>();
+        public string Nom { get; set; }
+        public Dossier(string nom)
+        {
+            Nom = nom;
+        }
+    }
+}
---
+++ Data/Fichier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Fichier
+    {
+        public string Nom { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateLastModification { get; set; }
+        public Fichier(string nom)
+        {
+            Nom = nom;
+            DateCreation = DateTime.Now;
+            DateLastModification = DateTime.Now;
+        }
+    }
+}
