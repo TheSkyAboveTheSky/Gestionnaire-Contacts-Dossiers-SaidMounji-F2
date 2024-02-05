80ec936cc47540c4e2baef9ba762603d3fdf91c0
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-02-05 20:01:34 +0100

    AjouterContact

--- ProjetCSharpGestionContactsDossiersSaidMounjiF2/Program.cs
+++ ProjetCSharpGestionContactsDossiersSaidMounjiF2/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using static System.Collections.Specialized.BitVector32;
+using System.Data;
+using System.Globalization;
 
 namespace ProjetCSharpGestionContactsDossiersSaidMounjiF2
 {
@@ -19,18 +21,27 @@ namespace ProjetCSharpGestionContactsDossiersSaidMounjiF2
             commandes = new Commande[]
             {
                 new Commande("aide", "aide [<nom de la commande>]", "Fournit de l'aide pour la commande spécifiée, ou liste les commandes si aucune n'est spécifiée.", Aide),
-                /*
                 new Commande("ajouterdossier", "ajouterdossier <nom>", "Ajoute un nouveau dossier au dossier actuel", AjouterDossier),
                 new Commande("ajoutercontact", "ajoutercontact <nom> <prenom> <adresse> <numero de telephone> <e-mail> <entreprise> <sexe: homme|femme> <relation: ami|famille|collegue|connaissance|autre>", "Ajoute un nouveau contact au dossier actuel", AjouterContact),
+                /*
                 new Commande("supprimerdossier", "supprimerdossier <nom>", "Supprime un dossier par nom du dossier actuel", SupprimerDossier),
+                /*
                 new Commande("supprimercontact", "supprimercontact <nom>", "Supprime un contact par nom du dossier actuel", SupprimerContact),
+                /*
                 new Commande("modifierdossier", "modifierdossier <nom> nom=<nouveau-nom>", "Met à jour un dossier par nom du dossier actuel.", ModifierDossier),
+                /*
                 new Commande("modifiercontact", "modifiercontact <nom> nom=<nouveau-nom> [prenom=<nouveau-prénom>] [adresse=<nouvelle-adresse>] [telephone=<nouveau-numéro-de-téléphone>] [email=<nouvelle-adresse-e-mail>] [entreprise=<nouvelle-entreprise>] [sexe=<nouveau-sexe: homme|femme>] [relation=<nouvelle-relation: ami|famille|collegue|connaissance|autre>]", "Met à jour un contact par nom du dossier actuel.", ModifierContact),
+                /*
                 new Commande("afficher", "afficher [.]", "Affiche l'arborescence complète.", Afficher),
+                /*
                 new Commande("charger", "charger [<mot de passe>]", "Charge l'arborescence à partir d'une sauvegarde si elle existe, en utilisant le mot de passe spécifié (facultatif)", Charger),
+                /*
                 new Commande("enregistrer", "enregistrer [<mot de passe>]", "Enregistre l'arborescence, chiffrée en utilisant le mot de passe spécifié (facultatif)", Enregistrer),
+                /*
                 new Commande("cd", "cd [<chemin relatif>]", "Change le répertoire de travail actuel vers celui spécifié, ou vers la racine s'il n'y en a pas", Cd),
+                /*
                 new Commande("pwd", "pwd", "Affiche le chemin absolu du répertoire de travail actuel", Pwd),
+                /*
                 new Commande("sortir", "sortir", "Quitte le programme sans enregistrer.", Sortir),
                 */
             };
@@ -40,7 +51,6 @@ namespace ProjetCSharpGestionContactsDossiersSaidMounjiF2
         {
             new Program().Run();
         }
-
         private void Run()
         {
             while (true)
@@ -114,5 +124,70 @@ namespace ProjetCSharpGestionContactsDossiersSaidMounjiF2
             return false;
 
         }
+
+        private bool AjouterDossier(string[] args)
+        {
+            if (args.Length != 2) return true;
+            try
+            {
+                var nomDossier = args[1];
+                gestionnaire.AjouterDossier(nomDossier);
+                Console.WriteLine($"Dossier \"{nomDossier}\" ajouté avec succès.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"AjouterDossier: {e.Message}");
+            }
+            catch (DuplicateNameException e)
+            {
+                Console.WriteLine($"AjouterDossier: {e.Message}");
+            }
+            return false;
+        }
+
+        private bool AjouterContact(string[] args)
+        {
+            if (args.Length != 9)
+            {
+                return true;
+            }
+
+            try
+            {
+                var nom = args[1];
+                var prenom = args[2];
+                var adresse = args[3];
+                var telephone = args[4];
+                var email = args[5];
+                var entreprise = args[6];
+                var sexe = args[7];
+                var relation = args[8];
+
+                if (!Enum.TryParse(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(relation), out Relation relationEnum))
+                {
+                    Console.WriteLine("AjouterContact: relation invalide, tapez \"aide ajoutercontact\" pour voir les relations disponibles");
+                    return false;
+                }
+
+                if (!Enum.TryParse(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(sexe), out Sexe sexeEnum))
+                {
+                    Console.WriteLine("AjouterContact: sexe invalide, tapez \"aide ajoutercontact\" pour voir les sexes disponibles");
+                    return false;
+                }
+
+                gestionnaire.AjouterContact(nom, prenom, adresse, telephone, email, entreprise, sexeEnum, relationEnum);
+                Console.WriteLine($"Contact \"{nom}\" ajouté avec succès.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"AjouterContact: {e.Message}");
+            }
+            catch (DuplicateNameException e)
+            {
+                Console.WriteLine($"AjouterContact: {e.Message}");
+            }
+            return false;
+        }
+
     }
 }
