9f59c8e803cf0d9be4f612c6413a284d167e3c5a
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-02-05 19:02:21 +0100

    Class Commande

---
+++ ProjetCSharpGestionContactsDossiersSaidMounjiF2/Commande.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetCSharpGestionContactsDossiersSaidMounjiF2
+{
+    internal class Commande
+    {
+        public readonly string Nom;
+        public readonly string Description;
+        public readonly string Syntax;
+        private Func<string[], bool> Action { get; }
+
+        public Commande(string nom, string description, string syntax, Func<string[], bool> action)
+        {
+            Nom = nom;
+            Description = description;
+            Syntax = syntax;
+            Action = action;
+        }
+        public void Executer(string[] args)
+        {
+            if (Action(args)) Console.WriteLine($"Arguments invalides, tapez \"aide {Nom}\" pour voir comment utiliser {Nom}");
+
+        }
+        static public void Executer(string[] args, Commande[] commandes)
+        {
+            Commande commande = commandes.FirstOrDefault(c => c.Nom == args[0]);
+
+            if (commande == null) Console.WriteLine("Commande invalide, tapez \"aide\" pour voir la liste des commandes");
+            else commande.Executer(args);
+        }
+    }
+}
