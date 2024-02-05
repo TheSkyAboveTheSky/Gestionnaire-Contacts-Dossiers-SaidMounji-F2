c5a3084ca7ae4643c0e0cabb18073219652bfb25
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2024-02-05 02:38:55 +0100

    Use Factory For Serialization

---
+++ Serialize/SerializerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Serialize
+{
+    public enum SerializerType
+    {
+        XML,
+        Binary,
+    }
+
+    public static class SerializerFactory
+    {
+        public static Serializer<T> GetSerializer<T>(SerializerType type, string key) where T : class
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("La cle ne peut pas etre vide ou nulle", nameof(key));
+            }
+
+            switch (type)
+            {
+                case SerializerType.XML:
+                    return new XmlSerializer<T>(key);
+                case SerializerType.Binary:
+                    return new BinarySerializer<T>(key);
+                default:
+                    throw new ArgumentException("Type de serialiseur invalide", nameof(type));
+            }
+        }
+    }
+
+}
